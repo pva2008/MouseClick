4816c54e3dc9f63fe65ed827e93eb12ec3c5ce34
Vasili Pasmurtsev <pva2008@gmail.com>
2023-11-14 12:43:47 +0300

    update 10 min

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,16 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws AWTException, InterruptedException {
-        System.out.println("Enter time");
-        Scanner in = new Scanner(System.in);
-        System.out.print("Input a number: ");
-        int num = in.nextInt();
+//        System.out.println("Enter time");
+//        Scanner in = new Scanner(System.in);
+//        System.out.print("Input a number: ");
+//        int num = in.nextInt();
 
         for(;;) {
-            Thread.sleep(num);
+            Thread.sleep(600000);
             clickMouse(300, 300);
+            Thread.sleep(600000);
+            clickMouse(600, 600);
         }
 
 
