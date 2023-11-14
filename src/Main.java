dd9495a43cb17c6820460e9dd44064dacfcb257a
Vasili Pasmurtsev <pva2008@gmail.com>
2023-11-14 14:33:04 +0300

    refactor

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
 import java.awt.*;
+import java.sql.Timestamp;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
@@ -8,20 +10,23 @@ public class Main {
 //        System.out.print("Input a number: ");
 //        int num = in.nextInt();
 
-        for(;;) {
-            Thread.sleep(800000);
-            clickMouse(300, 300);
-            Thread.sleep(800000);
-            clickMouse(600, 600);
+        for (; ; ) {
+            clickMouse(300, 600, 800000);
         }
-
-
-
-
     }
-    public static void  clickMouse(int x, int y) throws AWTException
-    {
+
+
+    public static void clickMouse(int min, int max, int time) throws AWTException, InterruptedException {
+
         Robot robot = new Robot();
+        Random rn = new Random();
+
+        int x = rn.nextInt(max - min + 1) + min;
+        int y = rn.nextInt(max - min + 1) + min;
+
+        System.out.println("sleep + " + new Timestamp(System.currentTimeMillis()));
+        Thread.sleep(time);
+        System.out.println("change mouse + " + new Timestamp(System.currentTimeMillis()));
         robot.mouseMove(x, y);
     }
 }
